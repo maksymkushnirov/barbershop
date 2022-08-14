0d99ff4638048e1e01f9dec7040a95ae56e5bdd9
Maksym Kushnirov <maksymkushnirov@gmail.com>
2022-08-14 20:35:04 +0300

    modal window

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -8,8 +8,19 @@
     menuBtnRef.classList.toggle('is-open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
     mobileMenuRef.classList.toggle('is-open');
+    !expanded ? fixedModalOverflow() : closeModalOverflow();
+    // !expanded && setActiveLink(document.querySelectorAll('.navigation-link'));
   };
 
   menuBtnRef.addEventListener('click', toggleMenu);
   menuLinks.addEventListener('click', toggleMenu);
 })();
+
+// Fixed window when modal opened
+function fixedModalOverflow() {
+  document.querySelector('body').style.overflow = 'hidden';
+}
+
+function closeModalOverflow() {
+  document.querySelector('body').style.overflow = 'unset';
+}
